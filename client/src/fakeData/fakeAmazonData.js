bbafb1d7399fdd0f45ccded4629e07d402367976
Steven Del Rosario <stevendelro@pm.me>
2021-06-27 09:17:30 -0500

    created multiple sources of sample data for testing

---
+++ client/src/fakeData/fakeAmazonData.js
@@ -0,0 +1,21 @@
+export const fakeAmazonData = [
+  {
+    id: 'amazon0',
+    title:
+      'Apple A1181 MacBook 13.3" Laptop with Intel Core 2 Duo 1GB RAM 120GB HD',
+    price: '$80.00',
+    img: 'https://i.ebayimg.com/thumbs/images/g/2bcAAOSw1fxgx~ui/s-l225.webp',
+  },
+  {
+    id: 'amazon1',
+    title: '',
+    price: '$75.00',
+    img: undefined,
+  },
+  {
+    id: 'amazon2',
+    title: '2009 Apple MacBook Unibody White 2.26ghz Core 2 Duo 4gb RAM 13"',
+    price: '$84.99',
+    img: 'https://i.ebayimg.com/thumbs/images/g/~4wAAOSwXJ9glah-/s-l225.webp',
+  },
+];
---
+++ client/src/fakeData/fakeTargetData.js
@@ -0,0 +1,22 @@
+export const fakeTargetData = [
+  {
+    id: 'target0',
+    title:
+      '⭐ Apple MacBook A1181 13.3" White  Mid 2007 2.16GHz Core 2 Duo T7400 Laptop #L3',
+    price: '$110.00',
+    img: 'https://i.ebayimg.com/thumbs/images/g/KvAAAOSwNt1gu5Gi/s-l225.webp',
+  },
+  {
+    id: 'target1',
+    title:
+      'Apple MacBook Air 2008(A1237) 13" INTEL 1.6 GHz Core 2 Duo - GOOD BATTE*',
+    price: '$109.95',
+    img: 'https://i.ebayimg.com/thumbs/images/g/P2QAAOSwftNgyEJn/s-l225.webp',
+  },
+  {
+    id: 'target2',
+    title: '',
+    price: '$75.00',
+    img: undefined,
+  },
+];
