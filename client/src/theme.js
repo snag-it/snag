02b5d781e9ef6ab297d975bb7db47009c6c21af4
Steven Del Rosario <stevendelro@pm.me>
2021-06-27 08:28:34 -0500

    adjusted theme to resolve strict mode error in browser console

--- client/src/theme.js
+++ client/src/theme.js
@@ -1,5 +1,5 @@
 import { red } from '@material-ui/core/colors';
-import { createMuiTheme } from '@material-ui/core/styles';
+import { unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-ui/core/styles';
 
 // A custom theme for this app
 const theme = createMuiTheme({
