1e415148245113644a5d1adc012410ca216c5267
Steven Del Rosario <stevendelro@pm.me>
2021-06-28 12:04:16 -0500

    created seperate reducers for each retailer

---
+++ client/src/reducers/targetReducer.js
@@ -0,0 +1,61 @@
+import * as actions from '../actions/actions';
+
+const initialState = {
+  targetLoading: false,
+  targetProducts: [],
+};
+
+function targetReducer(state = initialState, { type, payload }) {
+  switch (type) {
+    case actions.FETCH_STARTED:
+      return {
+        ...state,
+        targetLoading: true,
+      };
+    case actions.FETCH_COMPLETE:
+      return {
+        ...state,
+        targetLoading: false,
+      };
+    case actions.FETCH_TARGET:
+      const modifiedTargetList = [];
+      payload.products.forEach(product => {
+        product['isFavorite'] = false;
+        modifiedTargetList.push(product);
+      });
+      return {
+        ...state,
+        targetProducts: modifiedTargetList,
+      };
+    case actions.MARK_FAVORITE:
+      const withMarkedFavorite = state.targetProducts;
+      if (payload.retailer === 'target') {
+        withMarkedFavorite.forEach(product => {
+          if (product.id === payload.productId) {
+            product['isFavorite'] = true;
+          }
+        });
+        return {
+          ...state,
+          targetProducts: withMarkedFavorite,
+        };
+      }
+    case actions.UNMARK_FAVORITE:
+      if (payload.retailer === 'target') {
+        const withUnmarkedFavorite = state.targetProducts;
+        withUnmarkedFavorite.forEach(product => {
+          if (product.id === payload.productId) {
+            product['isFavorite'] = false;
+          }
+        });
+        return {
+          ...state,
+          targetProducts: withUnmarkedFavorite,
+        };
+      }
+    default:
+      return state;
+  }
+}
+
+export default targetReducer;
