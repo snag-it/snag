99dfd6bd03d2aad4276a7e69a5d22898df617d17
jackiedouglass <jackie.douglass@icloud.com>
2021-07-01 17:02:19 -0500

    working on grid display of favorites

--- client/src/pages/Favorites/index.js
+++ client/src/pages/Favorites/index.js
@@ -4,10 +4,22 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import FavoritesList from '../../components/FavoritesList';
 import NavBar from '../../components/NavBar';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    margin: theme.spacing(10, 0),
+    width: '40%',
+    [theme.breakpoints.up('sm')]: {
+      width: '40%',
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: '60%',
+    },
+    [theme.breakpoints.down('xs')]: {
+      width: '80%',
+    },
   },
 }));
 
@@ -27,7 +39,7 @@ function FavoritesPage({ favorites }) {
   return (
     <div>
       <NavBar />
-      <Grid container direction='row' justify='center'>
+      <Grid container direction='row' justify='center' alignContent='center'>
         <FavoritesList productData={favorites} />
       </Grid>
     </div>
